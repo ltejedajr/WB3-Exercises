c535fea5c384692c89bdea755f173e58bda8c314
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-25 14:26:00 -0500

    Modified part_codes.js

--- Strings/part_codes.js
+++ Strings/part_codes.js
@@ -1,28 +1,41 @@
 "use strict";
 
-function getProductSupplier(partCode) {
-    let colonPos = partCode.indexOf(":");
-    let productSupplier = partCode.substring(0, colonPos);
+let partCode1 = ("ACME:123-L");
+let partCode2 = ("DI:12345-M");
+let partCode3 = ("ACE:1-12");
+
+    function getProductSupplier(partCode) {
+        let colonPos = partCode.indexOf(":");
+        let productSupplier = partCode.substring(0, colonPos);
+        
+        console.log(productSupplier);
+    }
     
-    console.log(partCode);
-}
-
-function getProductNo(partCode) {
-    let colonPos = partCode.lastIndexOf(":");
-    let dashPos = partCode.lastIndexOf("-");
-    let productNo = partCode.substring(colonPos + dashPos);
+    function getProductNo(partCode) {
+        let colonPos = partCode.lastIndexOf(":");
+        let dashPos = partCode.indexOf("-");
+        let productNo = partCode.substring(colonPos + 1, dashPos);
+        
+        console.log(productNo);
+    }
     
-    console.log(partCode);
-}
+    function getProductSize(partCode) {
+        let dashPos = partCode.lastIndexOf("-");
+        let productSize = partCode.substring(dashPos + 1);
+        
+        console.log(productSize);
+       
+    }
+    
+    
+getProductSupplier(partCode1)
+getProductNo(partCode1);
+getProductSize(partCode1);
 
+getProductSupplier(partCode2);
+getProductNo(partCode2);
+getProductSize(partCode2);
 
-function getProductSize(partCode) {
-    let dashPos = partCode.lastIndexOf("-");
-    let productSize = partCode.substring(dashPos + 1);
-
-    console.log(partCode);
-}
-
-getProductSupplier("ACME:123-L");
-getProductNo("DI:12345-M");
-getProductSize("ACE:1-12");
+getProductSupplier(partCode3);
+getProductNo(partCode3);
+getProductSize(partCode3);
