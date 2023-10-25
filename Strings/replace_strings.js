cca298d0201d755742b760103ec1d3e57c5a7468
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-25 17:21:39 -0500

    Modified replace_strings.js

--- Strings/replace_strings.js
+++ Strings/replace_strings.js
@@ -5,3 +5,9 @@ let message = "Our corporate offices are located in Dallas.";
 let newMessage = message.replace("Dallas", "Austin");
 
 console.log(newMessage);
+
+message = "Our corporate offices are located in Dallas.";
+
+newMessage = message.replace("Dallas", /DALLAS/i);
+
+console.log(newMessage);
