53d742ba4b333a61e9e3d779a391a0da896f6dd5
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-25 12:29:07 -0500

    Added part_codes.js

---
+++ Strings/part_codes.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function getProductSupplier(partCode) {
+    let colonPos = partCode.indexOf(":");
+    let productSupplier = partCode.substring(0, colonPos);
+    
+    console.log(partCode);
+}
+
+function getProductNo(partCode) {
+    let colonPos = partCode.lastIndexOf(":");
+    let dashPos = partCode.lastIndexOf("-");
+    let productNo = partCode.substring(colonPos + dashPos);
+    
+    console.log(partCode);
+    
+}
+
+
+function getProductSize(partCode) {
+    let dashPos = partCode.lastIndexOf("-");
+    let productSize = partCode.substring(dashPos + 1);
+
+    console.log(partCode);
+}
+
+
+getProductSupplier("ACME:123-L");
+getProductNo("DI:12345-M");
+getProductSize("ACE:1-12");
