e744376a9e3f8997324ae743e48514def0fa8ce9
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-26 12:33:06 -0500

    Added more_date_conversions.js

---
+++ Dates/DateScripts/more_date_conversions.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let birthday = new Date("11/13/1993");
+let momBirthday = new Date("12 20 1960");
+let dadBirthday = new Date(1965, 3, 20);
+
+console.log(birthday.toLocaleString());
+console.log(momBirthday.toLocaleString());
+console.log(dadBirthday.toLocaleString());
